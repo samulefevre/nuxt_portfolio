7ec7962cb9617dd4645befb6369d53d0171c2d80
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2021-10-14 23:38:18 +0200

    add header

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
+    <NavHeader class="text-green-500" />
     <h1 class="text-red-500">Hello {{ new Date().getFullYear() }}</h1>
   </div>
 </template>
 
 <script lang="ts" setup>
 import './assets/tailwind.css'
+import NavHeader from './components/nav/navHeader.vue';
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,12 @@ import { defineNuxtConfig } from 'nuxt3'
 export default defineNuxtConfig({
     build: {
         postcss: {
-            postcssOptions: {
-                plugins: {
-                    tailwindcss: {},
-                    autoprefixer: {},
-                }
-            }
+          postcssOptions: {
+            plugins: {
+              tailwindcss: {},
+              autoprefixer: {},
+            },
+          },
         },
-    }
+      },
 })
