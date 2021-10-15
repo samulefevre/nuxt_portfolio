918593fc8d4d4009b3714cebc558f76ec73fec70
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2021-10-15 06:19:32 +0200

    add svg social icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="text-blue-500 text-center flex flex-col h-full space-y-4 items-center justify-center">
-    <h1 class="text-2xl">Bonjour,</h1>
-    <h2 class="text-5xl">Samuel LEFEVRE</h2>
-    <h3 class="text-white text-6xl">Développeur web et mobile</h3>
+    <div
+        class="text-blue-500 text-center flex flex-col h-full space-y-4 items-center justify-center"
+    >
+        <h1 class="text-2xl">Bonjour,</h1>
+        <h2 class="text-5xl">Samuel LEFEVRE</h2>
+        <h3 class="text-white text-6xl">Développeur web et mobile</h3>
 
-    <button class="p-4 border-2 border-blue-500 rounded-md w-52">ABOUT</button>
+        <div class="py-8">
+            <button class="p-4 border-2 border-blue-500 rounded-md w-52">ABOUT</button>
+        </div>
 
-    <div>
-        <a href="https://www.malt.fr/profile/samuellefevre1" target="_blank" class="underline text-4xl" >Engagez moi sur Malt.fr !</a>
+        <div>
+            <a
+                href="https://www.malt.fr/profile/samuellefevre1"
+                target="_blank"
+                class="underline text-4xl"
+            >Engagez moi sur Malt.fr !</a>
+        </div>
+
+        <div>
+            <CommonsSocialIcons class="block mt-20 text-white" />
+        </div>
     </div>
-
-    <div>
-        <CommonsSocialIcons class="block mt-20 text-white" />
-    </div>
-  </div>
 </template>
 
