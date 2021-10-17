45717988e524a3a2ea459b9bb1c4df2cc1f49093
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2021-10-17 21:01:26 +0200

    add 404 page

--- components/nav/navHeader.vue
+++ components/nav/navHeader.vue
@@ -1,12 +1,12 @@
 <template>
-    <header class="p-4 max-w-lg mx-auto text-right">
+    <div class="p-4 max-w-lg mx-auto text-right">
         <nav class="flex flex-row space-x-4 justify-end text-white">
             <nuxt-link to="/">Home</nuxt-link>
             <nuxt-link to="/about">A propos</nuxt-link>
             <nuxt-link to="/portfolio">Portfolio</nuxt-link>
             <nuxt-link to="/contact">Contact</nuxt-link>
         </nav>
-    </header>
+    </div>
 </template>
 
 <style>
---
+++ pages/404.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Oups !</h1>
+        <div class="mx-auto text-xl w-72 sm:w-96">
+            <img src="assets/img/404.svg" />
+            <p class="my-4">La page que vous cherchez semble introuvable.</p>
+        </div>
+    </div>
+</template>
