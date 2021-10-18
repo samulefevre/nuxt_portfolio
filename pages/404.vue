3a21c2a2b11ebc68b7682ada67b30bec60c1a0ff
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2021-10-18 20:27:13 +0200

    move svg to public dir

--- pages/404.vue
+++ pages/404.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>Oups !</h1>
         <div class="mx-auto text-xl w-72 sm:w-96">
-            <img src="assets/img/404.svg" />
+            <img src="/img/404.svg" />
             <p class="my-4">La page que vous cherchez semble introuvable.</p>
         </div>
     </div>
