3cf2e730f2315f4fd03d59dba0451e38ecf63adb
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2023-09-18 23:02:32 +0200

    fix app one parent

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <NavHeader />
-  <main>
-    <NuxtPage class="py-8 min-h-[var(--main-height)]" />
-  </main>
+  <div>
+    <NavHeader />
+    <main>
+      <NuxtPage class="py-8 min-h-[var(--main-height)]" />
+    </main>
+  </div>
 </template>
