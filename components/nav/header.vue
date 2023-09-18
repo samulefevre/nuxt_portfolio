3970f0d885259f82acca2b6362a985e751dcb507
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2023-09-19 00:23:09 +0200

    add menu for mobile

--- components/nav/header.vue
+++ components/nav/header.vue
@@ -15,18 +15,34 @@ const links = [
     {
         name: 'Contact',
         path: '/contact',
-    },]
+    }
+]
+
+const isOpen = ref(false)
 
 </script>
 <template>
-    <header class="sticky w-full z-30 top-0 h-16 translate-all backdrop:blur-md bg-gray-900/75 border-b-2 border-gray-800">
-        <UContainer class="h-full">
-            <nav class="flex flex-row gap-x-10 items-center text-lg h-full">
+    <header class="sticky w-full z-30 top-0 sm:h-16 bg-gray-900 border-b-2 border-gray-800 py-4">
+        <UContainer class="h-full relative">
+            <nav class="flex flex-col sm:flex-row gap-y-2 sm:gap-x-10 items-start sm:items-center text-lg h-full relavive ">
+                <div class="text-center text-xl text-primary-500 font-bold leading-tight w-full sm:hidden">
+                    Samuel LEFEVRE
+                </div>
+
                 <ULink v-for="link in links" :key="link.name" :to="link.path"
-                    active-class="text-primary-500 bg-gray-800 px-4 py-1 rounded-lg" class="px-4">{{
-                        link.name }}
+                    active-class="text-primary-500 bg-gray-800 px-4 py-1 rounded-lg" class="px-4 my-2 sm:block" :class="{
+                        'hidden': !isOpen,
+                        'block': isOpen
+                    }">{{
+    link.name }}
                 </ULink>
+
+                <UIcon v-if="!isOpen" name="i-ic-baseline-menu" class="absolute right-4 top-1 sm:hidden"
+                    @click="isOpen = !isOpen" />
+                <UIcon v-if="isOpen" name="i-ic-baseline-close" class="absolute right-4 top-1 sm:hidden"
+                    @click="isOpen = !isOpen" />
             </nav>
+
         </UContainer>
     </header>
 </template>
