435c5c24819deb9ba1318c1133a0c781d4c3efd1
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2023-09-18 22:56:15 +0200

    fix

--- app.vue
+++ app.vue
@@ -1,9 +1,6 @@
 <template>
-  <UContainer>
-    <NavHeader />
-    <!-- flex flex-col max-w-7xl mx-auto pt-24 px-4 h-screen w-screen -->
-    <main>
-      <NuxtPage class="py-8 min-h-[var(--main-height)]" />
-    </main>
-  </UContainer>
+  <NavHeader />
+  <main>
+    <NuxtPage class="py-8 min-h-[var(--main-height)]" />
+  </main>
 </template>
