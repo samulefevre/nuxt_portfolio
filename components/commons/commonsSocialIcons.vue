785db2c5f4b8cdd163916656fb037ec0dc5228df
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2023-09-20 11:28:41 +0200

    add title to social links

--- components/commons/commonsSocialIcons.vue
+++ components/commons/commonsSocialIcons.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="flex flex-row space-x-6">
         <div>
-            <NuxtLink to="https://github.com/samulefevre" external>
+            <NuxtLink to="https://github.com/samulefevre" title="Github Logo" external>
                 <UIcon name="i-mdi-github" class="w-12 h-12" />
             </NuxtLink>
         </div>
         <div>
-            <NuxtLink to="https://twitter.com/samuel_lefevr" external>
+            <NuxtLink to="https://twitter.com/samuel_lefevr" title="Twitter logo" external>
                 <UIcon name="i-mdi-twitter" class="w-12 h-12" />
             </NuxtLink>
         </div>
         <div>
-            <NuxtLink to="https://www.linkedin.com/in/samuel-lefevre-591446161/" external>
+            <NuxtLink to="https://www.linkedin.com/in/samuel-lefevre-591446161/" title="Linkedin logo" external>
                 <UIcon name="i-mdi-linkedin" class="w-12 h-12" />
             </NuxtLink>
         </div>
